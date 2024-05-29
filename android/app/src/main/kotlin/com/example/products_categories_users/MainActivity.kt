5bf57fc239a56129c85884fa66a4f3f1af9425f4
khusantoy <tursunovkhusanboy@gmail.com>
2024-05-29 15:56:27 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/products_categories_users/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.products_categories_users
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/views/screens/home_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:products_categories_users/views/screens/categories_screen.dart';
+import 'package:products_categories_users/views/screens/products_screen.dart';
+import 'package:products_categories_users/views/screens/users_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 3, vsync: this, initialIndex: 0);
+    super.initState();
+    tabController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: tabController.index == 0
+            ? const Text("Categories")
+            : tabController.index == 1
+                ? const Text("Products")
+                : const Text("Users"),
+        centerTitle: true,
+        bottom: TabBar(controller: tabController, tabs: const [
+          Tab(
+            text: "Categories",
+            icon: Icon(Icons.category),
+          ),
+          Tab(
+            text: "Products",
+            icon: Icon(Icons.shopping_cart),
+          ),
+          Tab(
+            text: "Users",
+            icon: Icon(Icons.group),
+          )
+        ]),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: TabBarView(
+              controller: tabController,
+              children: const [
+                CategoriesScreen(),
+                ProductsScreen(),
+                UserScreen(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
